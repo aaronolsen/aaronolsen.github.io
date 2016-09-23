d6c286c0e85ee5eb460f38973afda5e06f9e5b11
Aaron Olsen <aaron@Aarons-MBP-3.fios-router.home>
2016-09-22 22:38:50 -0400

    File updated

--- js/sharedfunctions.js
+++ js/sharedfunctions.js
@@ -23,7 +23,7 @@ if(page_trail[0] == '') page_trail[0] = 'about_me';
 if(url_split[4] == 'aaron'){
 	if(url_split.length <= 6){url_level = 1}else{url_level = url_split.length - 9}
 }else{
-	if(url_split.length <= 5){url_level = 1}else{url_level = url_split.length - 4}
+	if(url_split.length <= 5){url_level = 1}else{url_level = url_split.length - 5}
 }
 
 fill_header()
